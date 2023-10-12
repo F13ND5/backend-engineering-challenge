c8b5d38b71890f1c68706a4f53462bede1d50318
F13ND5 <112640850+F13ND5@users.noreply.github.com>
2023-10-12 10:54:10 +0100

    first commit

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # Backend Engineering Challenge
 
+## My Solution
 
-Welcome to our Engineering Challenge repository 🖖
+To run the project just run the following command with the desired window size and input file:
 
-If you found this repository it probably means that you are participating in our recruitment process. Thank you for your time and energy. If that's not the case please take a look at our [openings](https://unbabel.com/careers/) and apply!
+	python unbabel_cli.py --input_file events.json --out_file output.json --window_size 10
 
-Please fork this repo before you start working on the challenge, read it careful and take your time and think about the solution. Also, please fork this repository because we will evaluate the code on the fork.
-
-This is an opportunity for us both to work together and get to know each other in a more technical way. If you have any questions please open and issue and we'll reach out to help.
-
-Good luck!
+```
+Usage of unbabel_cli:
+  --input_file or -in
+        to select the input file
+  --out_file or -out
+        to select the file for the output (not required | by default goes to "output.json")
+  --window_size or -w
+        number of minutes for which to calculate the moving average (default 10)
+```
 
 ## Challenge Scenario
 
@@ -68,19 +73,3 @@ The output file would be something in the following format.
 {"date": "2018-12-26 18:23:00", "average_delivery_time": 31}
 {"date": "2018-12-26 18:24:00", "average_delivery_time": 42.5}
 ```
-
-#### Notes
-
-Before jumping right into implementation we advise you to think about the solution first. We will evaluate, not only if your solution works but also the following aspects:
-
-+ Simple and easy to read code. Remember that [simple is not easy](https://www.infoq.com/presentations/Simple-Made-Easy)
-+ Comment your code. The easier it is to understand the complex parts, the faster and more positive the feedback will be
-+ Consider the optimizations you can do, given the order of the input lines
-+ Include a README.md that briefly describes how to build and run your code, as well as how to **test it**
-+ Be consistent in your code. 
-
-Feel free to, in your solution, include some your considerations while doing this challenge. We want you to solve this challenge in the language you feel most comfortable with. Our machines run Python (3.7.x or higher) or Go (1.16.x or higher). If you are thinking of using any other programming language please reach out to us first 🙏.
-
-Also, if you have any problem please **open an issue**. 
-
-Good luck and may the force be with you
---
+++ unbabel_cli.py
@@ -0,0 +1,117 @@
+# Import necessary modules
+from argparse import ArgumentParser, ArgumentTypeError
+import math
+from pathlib import Path
+from datetime import datetime, timedelta
+import json
+from statistics import mean 
+
+# Define a function to parse command-line arguments
+def parse_args():
+    # Create an ArgumentParser object
+    parser = ArgumentParser(
+        prog="moving average of the translation delivery time", # name of program
+        description="Takes a json file and a window time, loads the json, calculate for every minute, a moving average of the translation delivery time for the last X minutes and then saves the json to specified file.", # description
+        epilog="By Bruno Carvalho" # bottom text
+    )
+    
+    # add arguments
+    parser.add_argument("-in", "--input_file", dest="in_file", help="The input file to change the separator in") # The input file argument
+    parser.add_argument("-out", "--out_file", dest="out_file", help="The output file(s) to write to") # output file
+    parser.add_argument("-w", "--window_size", dest="window_size", type=int, default=10, help="Window time") # window time
+    
+    return parser.parse_args()
+
+
+def average(lst) -> float:
+	return round(mean(lst), 1) if lst else 0
+
+
+def check_window_size(window_size):
+    if window_size < 0 or not isinstance(window_size, int):
+        raise ArgumentTypeError("'window_size' must be a positive integer.")
+
+
+def load_data_from_file(file_path):
+    with open(file_path) as f:
+        data = json.load(f)
+    return data
+
+
+def save_payload_to_file(out_file, overwrite, payload):
+    if not out_file:
+        out_file = "output.json"
+ 
+    if not overwrite and Path(out_file).exists():
+        raise ValueError("overwrite was not specified and out-file already exists.")
+ 
+    with open(out_file, "w") as output_file:
+        json.dump(payload, output_file, indent=1)
+
+
+# Define a function to calculate the moving average delivery time
+def calculate_moving_average(data, window_size):
+    
+    start_time = datetime.strptime(data[0]['timestamp'],'%Y-%m-%d %H:%M:%S.%f')  # Timestamp first translation
+    end_time = datetime.strptime(data[-1]['timestamp'],'%Y-%m-%d %H:%M:%S.%f') # Timestamp last translation
+    delta = (end_time - start_time).total_seconds() /60 # Number of minutes between first and last translation
+    time_window = timedelta(minutes = window_size)
+
+    lst_durations = [] # Stores relevant translation durations 
+    final_payload = [] # Stores final data to dump in json file
+
+    index_append  = 0 # Index in list pointing at the next translation to be included in the average
+    index_pop = 0 # Index in list  pointing at the at next translation to be removed from the average
+
+    WINDOW_INCREMENT = 1
+    MINUTE_INCREMENT = 1
+
+    #delta = delta + 1 if delta < (end_time.minute - start_time.minute) else delta
+		
+	# For each minute, check if translation event should be either included or excluded
+    for minute in range(math.ceil(delta) + WINDOW_INCREMENT):
+        
+        curr_time = start_time.replace(second=0, microsecond=0) + timedelta(minutes=minute)
+
+		# Selecting the next translation to be included in the average
+        for d in data[index_append:]:
+
+            time = datetime.strptime(d['timestamp'],'%Y-%m-%d %H:%M:%S.%f')
+
+            if curr_time >= time:
+                lst_durations.append(d['duration'])
+                index_append += 1
+            else:
+                break
+
+		# Selecting the next translation to be removed from the average
+        for d in data[index_pop:]:
+
+            time = datetime.strptime(d['timestamp'],'%Y-%m-%d %H:%M:%S.%f')
+
+            if (curr_time - time_window) >= time:
+                lst_durations.pop(0)
+                index_pop += 1
+            else:
+                break
+		
+        event = {"date" : str(curr_time), "average_delivery_time" : average(lst_durations)}
+        final_payload.append(event)
+
+        curr_time += timedelta(minutes = MINUTE_INCREMENT)
+        
+    return final_payload
+    
+
+def main(in_file, out_file, window_size, overwrite=False):
+    
+    data = load_data_from_file(in_file)
+    check_window_size(window_size)
+    payload = calculate_moving_average(data, window_size)
+    save_payload_to_file(out_file, overwrite, payload)
+
+
+if __name__ == "__main__":
+   
+    args = parse_args()
+    main(**vars(args))
